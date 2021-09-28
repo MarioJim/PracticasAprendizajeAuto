9da8cc0cd2839fed3a2eac2ddd10d5b4dca7a043
Abraham Haros <abrahamharos@Abrahams-MacBook-Pro.local>
2021-09-27 23:09:22 -0700

    Add plot support and dataset selection

--- P5/practice5.py
+++ P5/practice5.py
@@ -1,12 +1,38 @@
 import os
-
+import sys
 from graphviz import Source
 import numpy as np
 import matplotlib as mpl
 import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap
 from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier, export_graphviz
+
+def plot_decision_boundary(clf, X, y, axes=[0, 7.5, 0, 3], iris=True, legend=False, plot_training=True):
+    x1s = np.linspace(axes[0], axes[1], 100)
+    x2s = np.linspace(axes[2], axes[3], 100)
+    x1, x2 = np.meshgrid(x1s, x2s)
+    X_new = np.c_[x1.ravel(), x2.ravel()]
+    y_pred = clf.predict(X_new).reshape(x1.shape)
+    custom_cmap = ListedColormap(['#fafab0','#9898ff','#a0faa0'])
+    plt.contourf(x1, x2, y_pred, alpha=0.3, cmap=custom_cmap)
+    if not iris:
+        custom_cmap2 = ListedColormap(['#7d7d58','#4c4c7f','#507d50'])
+        plt.contour(x1, x2, y_pred, cmap=custom_cmap2, alpha=0.8)
+    if plot_training:
+        plt.plot(X[:, 0][y==0], X[:, 1][y==0], "yo", label="Iris setosa")
+        plt.plot(X[:, 0][y==1], X[:, 1][y==1], "bs", label="Iris versicolor")
+        plt.plot(X[:, 0][y==2], X[:, 1][y==2], "g^", label="Iris virginica")
+        plt.axis(axes)
+    if iris:
+        plt.xlabel("Petal length", fontsize=14)
+        plt.ylabel("Petal width", fontsize=14)
+    else:
+        plt.xlabel(r"$x_1$", fontsize=18)
+        plt.ylabel(r"$x_2$", fontsize=18, rotation=0)
+    if legend:
+        plt.legend(loc="lower right", fontsize=14)
 
 mpl.rc('axes', labelsize=14)
 mpl.rc('xtick', labelsize=12)
@@ -26,23 +52,37 @@ def save_fig(fig_id, tight_layout=True, fig_extension="png", resolution=300):
         plt.tight_layout()
     plt.savefig(path, format=fig_extension, dpi=resolution)
 
+# Dataset selection
+if sys.argv[1] == 'iris':
+    iris = load_iris()
+    feauture_names = iris.feature_names[-2:]
+    treeFilename = 'iris_tree'
+    X = iris.data[:, -2:]
+    y = iris.target
 
-# Load and split Iris dataset
-iris = load_iris()
 xTrain, xTest, yTrain, yTest = train_test_split(
-    iris.data, iris.target, random_state=0)
+    X, y, random_state=0)
 
 # Create and train the decision tree model
 tree_clf = DecisionTreeClassifier(random_state=0)
 tree_clf.fit(xTrain, yTrain)
 
 # Export an image of the tree
-dot_src = export_graphviz(tree_clf, feature_names=iris.feature_names,
+dot_src = export_graphviz(tree_clf, feature_names=feauture_names,
                           class_names=iris.target_names, rounded=True,
                           filled=True)
-image_filename = os.path.join(IMAGES_PATH, "iris_tree")
+image_filename = os.path.join(IMAGES_PATH, treeFilename)
 Source(dot_src).render(image_filename, format="png", cleanup=True)
 
 # Test the decision tree
 accuracy = tree_clf.score(xTest, yTest)
 print("Accuracy:", accuracy)
+
+plt.figure(figsize=(8, 4))
+plot_decision_boundary(tree_clf, xTest, yTest)
+plt.plot([2.45, 2.45], [0, 3], "k-", linewidth=2)
+plt.plot([2.45, 7.5], [1.75, 1.75], "k--", linewidth=2)
+plt.text(1.40, 1.0, "Depth=0", fontsize=15)
+plt.text(3.2, 1.80, "Depth=1", fontsize=13)
+save_fig("decision_tree_decision_boundaries_plot")
+plt.show()
